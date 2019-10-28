80530869486c296ef868c0a7e513040608a84fe0
Guilherme Mossi <guilhermeissom@outlook.com>
2019-10-28 11:11:00 -0300

    Exercicio4 es6 part 1

--- js/bundle.js
+++ js/bundle.js
@@ -1,35 +1,29 @@
 "use strict";
 
-// 3.1
-var arr = [1, 2, 3, 4, 5];
-console.log(arr.map(function (item) {
-  return item + 10;
-})); // 3.2
-// Dica: Utilize uma constante pra function
-
-/*const usuario = { nome: 'Diego', idade: 23 };
-const {idade} = usuario;
-console.log(idade);*/
-// 3.3
-// Dica: Utilize uma constante pra function
-
-var nome = "Diego";
-var idade = 23;
-
-var mostraUsuario = function mostraUsuario() {
-  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
-  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
-  return {
-    nome: nome,
-    idade: idade
-  };
+var empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC'
+  }
 };
+var nome = empresa.nome,
+    cidade = empresa.endereco.cidade,
+    estado = empresa.endereco.estado;
+console.log(nome); // Rocketseat
 
-console.log(mostraUsuario(nome, idade));
-console.log(mostraUsuario(nome)); // 3.4
+console.log(cidade); // Rio do Sul
 
-var promise = function promise() {
-  return new Promise(function (resolve, reject) {
-    return resolve();
-  });
-};
+console.log(estado); // SC
+
+function mostraInfo(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  // return `${usuario.nome} tem ${usuario.idade} anos.`;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
+}
+
+console.log(mostraInfo({
+  nome: 'Diego',
+  idade: 23
+})); //return `${nome} tem ${idade} anos.`;
