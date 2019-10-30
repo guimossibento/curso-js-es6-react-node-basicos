bf6985bb90c81b53c995b86ea7962618d49bca91
Guilherme Mossi <guilhermeissom@outlook.com>
2019-10-30 15:13:55 -0300

    Exercicios es6 parte2

--- Atividades/teoriaAsyc_await.js
+++ Atividades/teoriaAsyc_await.js
@@ -1,17 +1,17 @@
-const minhaPromisse = () => new Promise((resolve, reject)=>{
-    setTimeout(()=>{resolve('Ok')}, 2000);
+const minhaPromisse = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('Ok') }, 2000);
 });
 /*
 minhaPromisse().then(response =>{
     console.log(response);
 });*/
 
- const executaPromise = async () => {
+const executaPromise = async () => {
     console.log(await minhaPromisse());
     console.log(await minhaPromisse());
     console.log(await minhaPromisse());
 }
- 
+
 
 
 executaPromise();
