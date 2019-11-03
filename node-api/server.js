9ab61d3944e07d2bf7c06badec1c1cd19eaca494
Guilherme Mossi <guilhermeissom@outlook.com>
2019-11-03 16:03:01 -0300

    Node API

--- node-api/server.js
+++ node-api/server.js
@@ -1,27 +1,22 @@
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir')
 
 //Iniciando APP
 const app = express();
+app.use(express.json());
 
 //Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi');
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
+//Rotas
 
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React',
-        description: 'building',
-        url: 'https://github.com'
-    });
-    
-    return res.send('Hello MY OLD FRIE');
-});
+app.use('/api', require('./src/routes'))
 
-app.listen(3001);
+app.listen(3002);
 
 
